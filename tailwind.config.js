26d50b1e3f6a56777245ade5425ded860e1ae94d
robert-95 <robert.schleinhege@ionos.com>
2021-05-01 10:24:51 +0200

    heading font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Dosis', 'sans-serif'],
+        heading: ['Pacifico', 'sans-serif'],
+    },
+    },
   },
   variants: {
     extend: {},
